1e401fddd1c6af2a49c44802bdd84fb157a2de81
JinyaX <ttwotu@qq.com>
2018-04-06 00:12:21 +0800

    TTLog updated

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
 # TTLog
-supplements to the output of 'print' function with date+file+line+function and log type
+Supplements to the output of `print` function with date+file+line+function and log type.
 
 
-NOT COMPLETED
+# Installation
+Download and drag the file `TTLog.swift` to your Xcode project.
+
+# Usage
+code like this: 
+```
+TTLog.verbose(" This a verbose message!!!")
+TTLog.info("Some infomation to show!")
+TTLog.warning("These issues we must not overlook!!")
+TTLog.debug("We meet the bug here!!")
+TTLog.error("Here is an error!!!")
+```
+and console outputs: 
+```
+[2018-04-05 23:53:36.469] ViewController.swift:18 viewDidLoad() ❌ERROR: Here is an error!!!
+[2018-04-05 23:53:36.469] ViewController.swift:20 viewDidLoad() ⚠️WARNING: These issues we must not overlook!!
+[2018-04-05 23:53:36.469] ViewController.swift:17 viewDidLoad() 👻DEBUG: We meet the bug here!
+[2018-04-05 23:53:36.469] ViewController.swift:19 viewDidLoad() ℹ️INFO: Some infomation to show!
+[2018-04-05 23:53:36.469] ViewController.swift:16 viewDidLoad() 📣VERBOSE:  This a verbose message!!!
+```
+
+# Customization
+You can easily modify `TTLog.swift` easily to create your own `print` func.
+
+# License
+MIT License, see the LICENSE file for more infomation.
+
+
--- TTLog.swift
+++ TTLog.swift
@@ -10,6 +10,9 @@ import Foundation
 
 struct TTLog {
     
+    private static let logDateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+    private static let logTextFormat = "[%@] %@:%@ %@ %@: %@"
+    
     public enum TTLogType {
         case verbose, info, warning, debug, error
         
@@ -17,74 +20,62 @@ struct TTLog {
             return String(describing: self).uppercased()
         }
         
-        var stringWithColor: String {
+        var tagText: String {
             switch self {
-            case .verbose: return self.description.withColor("#C8C8C8")
-            case .info:    return self.description.withColor("#0000FF")
-            case .warning: return self.description.withColor("#00FF00")
-            case .debug:   return self.description.withColor("#FFFB00")
-            case .error:   return self.description.withColor("#FF0000")
+            case .verbose: return "📣" + self.description
+            case .info:    return "ℹ️" + self.description
+            case .warning: return "⚠️" + self.description
+            case .debug:   return "👻" + self.description
+            case .error:   return "❌" + self.description
             }
         }
     }
     
-    public func verbose(_ items: Any..., separator: String = "", terminator: String = "\n", file: String = #file, line: Int = #line, function: String = #function) {
+    public static func verbose(_ items: Any..., separator: String = " ", terminator: String = "\n", file: String = #file, line: Int = #line, function: String = #function) {
         log(.verbose, items, separator, terminator, file, line, function)
     }
     
-    public func info(_ items: Any..., separator: String = "", terminator: String = "\n", file: String = #file, line: Int = #line, function: String = #function) {
+    public static func info(_ items: Any..., separator: String = " ", terminator: String = "\n", file: String = #file, line: Int = #line, function: String = #function) {
         log(.info, items, separator, terminator, file, line, function)
     }
     
-    public func warning(_ items: Any..., separator: String = "", terminator: String = "\n", file: String = #file, line: Int = #line, function: String = #function) {
+    public static func warning(_ items: Any..., separator: String = " ", terminator: String = "\n", file: String = #file, line: Int = #line, function: String = #function) {
         log(.warning, items, separator, terminator, file, line, function)
     }
     
-    public func debug(_ items: Any..., separator: String = "", terminator: String = "\n", file: String = #file, line: Int = #line, function: String = #function) {
+    public static func debug(_ items: Any..., separator: String = " ", terminator: String = "\n", file: String = #file, line: Int = #line, function: String = #function) {
         log(.debug, items, separator, terminator, file, line, function)
     }
     
-    public func error(_ items: Any..., separator: String = "", terminator: String = "\n", file: String = #file, line: Int = #line, function: String = #function) {
+    public static func error(_ items: Any..., separator: String = " ", terminator: String = "\n", file: String = #file, line: Int = #line, function: String = #function) {
         log(.error, items, separator, terminator, file, line, function)
     }
     
-    private func log(_ type: TTLogType ,_ items: [Any], _ separator: String, _ terminator: String, _ file: String, _ line: Int, _ function: String) {
-//        #if DEBUG
-//        #endif
-        let date = Date()
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
-        let dateString = dateFormatter.string(from: date)
-        let fileString = file.lastPathComponent
-        let lineString = line
-        let columnString = column
-        let typeString = type.stringWithColor
-        let message = items.map({ String(describing: $0) }).joined(separator: separator)
-        
-        // Coming soon
+    private static func log(_ type: TTLogType ,_ items: [Any], _ separator: String, _ terminator: String, _ file: String, _ line: Int, _ function: String) {
+        #if DEBUG
+        DispatchQueue(label: "TTLog").async {
+            let date = Date()
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = logDateFormat
+            let dateString = dateFormatter.string(from: date)
+            
+            let arguments: [CVarArg] = [dateString,
+                                        file.lastPathComponent,
+                                        String(line),
+                                        function,
+                                        type.tagText,
+                                        items.map({ String(describing: $0) }).joined(separator: separator),
+                                        ]
+            let result = String(format: logTextFormat, arguments: arguments) + terminator
+            Swift.print(result, separator: "", terminator: "")
+        }
+        #endif
     }
 }
 
 extension String {
-    // The last path component of the receiver.
     var lastPathComponent: String {
         return NSString(string: self).lastPathComponent
-    }
- 
-    // A new string made by deleting the extension from the receiver.
-    var stringByDeletingPathExtension: String {
-        return NSString(string: self).deletingPathExtension
-    }
- 
-    /**
-     Returns a string colored with the specified color.
- 
-     - parameter color: The string representation of the color. e.g. #
- 
-     - returns: A string colored with the specified color.
-     */
-    func withColor(_ color: String) -> String {
-        return "\u{001b}[fg\(color);\(self)\u{001b}[;"
     }
 }
 
